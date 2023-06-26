561f84df76a0af42f828d8769529186557921a14
Jesus Ramos <jesusram214.jr@gmail.com>
2023-06-25 22:47:06 -0500

    Changes

---
+++ Exercise 1.py
@@ -0,0 +1,8 @@
+user_name = input("Please enter your name: ")
+user_age = int(input("Please enter your age: "))
+
+calculated_year = (2023 - user_age) + 100
+print("Hello " + user_name + ", you will turn 100 years old in " + str(calculated_year))
+
+
+
