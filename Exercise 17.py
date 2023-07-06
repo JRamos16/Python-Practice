313e47d7f3af902d62c2924de1e9e67d698baeff
Jesus Ramos <jesusram214.jr@gmail.com>
2023-07-05 21:57:24 -0500

    Jason fumbled

--- Exercise 17.py
+++ Exercise 17.py
@@ -1,4 +1,11 @@
 import requests
-url = 'http://github.com'
-r = requests.get(url)
-r_html = r.text
+from bs4 import BeautifulSoup
+
+base_url = 'https://www.nytimes.com/'
+r = requests.get(base_url)
+soup = BeautifulSoup(r.text,'html.parser')
+
+# print(soup.contents)
+print(soup.find_all("div", {"class": "css-plkyuz"}))
+# for story_heading in soup.find_all(class_="indicate-hover css-on97le"):
+#     print(story_heading)
